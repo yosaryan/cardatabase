f99f5cbe3964eb96dbd9d3d91d44988aa5c49ed4
yosaryan <zhivko.zhelyazkovv@gmail.com>
2020-09-12 13:56:45 +0300

    car controller class

---
+++ src/main/java/com/packt/cardatabase/web/CarController.java
@@ -0,0 +1,18 @@
+package com.packt.cardatabase.web;
+
+import com.packt.cardatabase.domain.Car;
+import com.packt.cardatabase.domain.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CarController {
+    @Autowired
+    private CarRepository repository;
+
+    @RequestMapping ("/cars")
+    public Iterable<Car> getsCars() {
+    return repository.findAll();
+    }
+}
