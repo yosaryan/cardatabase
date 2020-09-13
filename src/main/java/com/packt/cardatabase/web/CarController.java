df39136153e521d53ba4fdd9160e1916cdcd5b7a
yosaryan <zhivko.zhelyazkov2010@gmail.com>
2020-09-13 16:21:06 +0300

    refactor postman collection and add new endpoint find by brand

--- src/main/java/com/packt/cardatabase/web/CarController.java
+++ src/main/java/com/packt/cardatabase/web/CarController.java
@@ -3,8 +3,15 @@ package com.packt.cardatabase.web;
 import com.packt.cardatabase.domain.cars.Car;
 import com.packt.cardatabase.domain.cars.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CarController {
@@ -14,8 +21,14 @@ public class CarController {
 
     @RequestMapping("/cars")
     public Iterable<Car> getCars() {
-
         return repository.findAll();
-
     }
+
+    @GetMapping("/cars/brand/{brand}")
+    public ResponseEntity<List<Car>> findByBrand(@PathVariable("brand") String brand) {
+        List<Car> list = repository.findByBrand(brand);
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(list);
+    }
+
+
 }
