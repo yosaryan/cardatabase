49831a4a410c5ae01bd4a65268d0d9992743a2dc
yosaryan <zhivko.zhelyazkov2010@gmail.com>
2020-09-13 15:34:47 +0300

    add postman collection

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -28,15 +28,15 @@ public class CardatabaseApplication {
 	CommandLineRunner runner () {
 		return args -> {
 		// Add owner objects and save these to db
-			Owner owner1 = new Owner ("John", "Johnson");
-			Owner owner2 = new Owner ("Mary", "Robinson");
-			orepository.save(owner1);
-			orepository.save(owner2);
+//			Owner owner1 = new Owner ("John", "Johnson");
+//			Owner owner2 = new Owner ("Mary", "Robinson");
+//			orepository.save(owner1);
+//			orepository.save(owner2);
 
 		// Add car object with link to owners and save these to db.
-		repository.save (new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
-		repository.save (new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
-		repository.save (new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
+//		repository.save (new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
+//		repository.save (new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
+//		repository.save (new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
 		};
 	}
 
