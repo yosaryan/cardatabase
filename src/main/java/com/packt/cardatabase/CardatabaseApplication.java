a2b7a4c1a7debdb1c46eed45b15c24b649a3a8cf
yosaryan <zhivko.zhelyazkov2010@gmail.com>
2020-10-13 21:52:41 +0300

    added security

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -1,6 +1,7 @@
 package com.packt.cardatabase;
 
 
+import com.packt.cardatabase.domain.User;
 import com.packt.cardatabase.domain.cars.Car;
 import com.packt.cardatabase.domain.cars.CarRepository;
 import com.packt.cardatabase.domain.users.Owner;
@@ -37,6 +38,9 @@ public class CardatabaseApplication {
 //            repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
 //            repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
 //            repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
+
+//            urepository.save(new User("user", "$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi", "USER"));
+//            urepository.save(new User("admin", "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG", "ADMIN"));
         };
     }
 
