3a6288711c2d512762f4d5a1be9fda35937eb928
yosaryan <zhivko.zhelyazkovv@gmail.com>
2020-10-19 16:16:48 +0300

    backend deployment

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 
@@ -24,14 +22,14 @@ public class CardatabaseApplication {
     @Autowired
     private UserRepository userRepository;
 
-    @SpringBootApplication
-    public class Application extends SpringBootServletInitializer {
-        @Override
-        protected SpringApplicationBuilder configure
-                (SpringApplicationBuilder application) {
-            return application.sources(Application.class);
-        }
-    }
+//    @SpringBootApplication
+//    public class Application extends SpringBootServletInitializer {
+//        @Override
+//        protected SpringApplicationBuilder configure
+//                (SpringApplicationBuilder application) {
+//            return application.sources(Application.class);
+//        }
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(CardatabaseApplication.class, args);
