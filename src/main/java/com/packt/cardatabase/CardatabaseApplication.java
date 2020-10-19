9765feeac086194240f8a4dff24304dbe0d3688b
yosaryan <zhivko.zhelyazkov2010@gmail.com>
2020-10-19 14:17:26 +0300

    typos fixed

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -32,13 +32,13 @@ public class CardatabaseApplication {
             // Add owner objects and save these to db
 //            Owner owner1 = new Owner("John", "Johnson");
 //            Owner owner2 = new Owner("Mary", "Robinson");
-//            orepository.save(owner1);
-//            orepository.save(owner2);
+//            ownerRepository.save(owner1);
+//            ownerRepository.save(owner2);
 
             // Add car object with link to owners and save these to db.
-//            repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
-//            repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
-//            repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
+//            carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
+//            carRepository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
+//            carRepository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
 
 //            userRepository.save(new User("user", "$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi", "USER"));
 //            userRepository.save(new User("admin", "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG", "ADMIN"));
